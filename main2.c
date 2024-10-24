acf139d7740e191480ee2f05495385213a69ae89
ikons <ikons@cslab.ece.ntua.gr>
2024-10-24 18:39:13 +0300

    write output to file in the local directory

--- main2.c
+++ main2.c
@@ -9,6 +9,8 @@
 
 int main() 
 { 
+   FILE *fp;
+   fp = fopen("novowels.txt","w+");
    setlocale(LC_ALL, "");
    wchar_t novowelsStr[] = L"Θα βγάλω όλα τα φωνήεντα από αυτή την συμβολοσειρά.";
    wchar_t originalString[256];   
@@ -17,6 +19,8 @@ int main()
    wprintf(L"palindrome (\"noon\") = %d \n", palindrome("noon") );
    removevowel(novowelsStr);
    wprintf(L"removevowel (\"%ls\") = %ls \n",originalString, novowelsStr );
+   fwprintf(fp, novowelsStr);
+   fclose(fp);
    return 0; 
 }
 
